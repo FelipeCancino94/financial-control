c16d12890e9eb3a026c9155f6e62fb9b5ee0d626
Laura Lopez <lauralop_11@hotmail.com>
2023-08-21 11:15:17 -0400

    primeras funciones

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,48 @@
-"use strict";
+/*"use strict";
 var mensaje = 'hola mundo';
 console.log(mensaje);
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=index.js.map*/
+
+// Variables y Selectores
+const formulario = document.querySelector('#presupuesto');
+const gastoListado = document.querySelector('#gasto');
+const selectorMes = document.querySelector('#selectMes');
+
+
+
+function eventListener(){
+    selectorMes.addEventListener ('change', preguntarPresupuesto);   
+}
+eventListener();
+
+
+class Presupuesto {
+    constructor (presupuesto){
+        this.total = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gasto = [];
+    }
+}
+class UI {
+    insertarPresupuesto(cantidad){
+        const {total, restante } = cantidad;
+        document.querySelector('#total').textContent = total;
+        document.querySelector('#restante').textContent = restante;
+    }
+}
+const ui = new UI();
+let presupuesto;
+
+function preguntarPresupuesto(){
+    const presupuestoIngresado = prompt('Cual es el presupuesto?');
+    
+    
+    if (presupuesto === '' || presupuesto === null || presupuesto <= 0){
+        alert ('Ingrese una cantidad valida');
+    }
+
+    presupuesto = new Presupuesto (presupuestoIngresado);
+    console.log(presupuesto);
+
+    ui.insertarPresupuesto(presupuesto);
+}
