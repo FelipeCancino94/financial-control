339c1813ecdbb766822eb42db51ad55923d37e8c
lauralop11 <lauralop_11@hotmail.com>
2023-09-21 06:06:35 -0400

    mensaje error y estilos

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,8 @@ console.log(mensaje);
 //# sourceMappingURL=index.js.map*/
 
 // Variables y Selectores
-const formulario = document.querySelector('#presupuesto');
-const gastoListado = document.querySelector('#gasto');
+const gastoMensual = document.querySelector('#presupuesto');
+const gastoListado = document.querySelector('#nombre-gasto');
 const selectorMes = document.querySelector('#selectMes');
 
 
@@ -29,6 +29,29 @@ class UI {
         document.querySelector('#total').textContent = total;
         document.querySelector('#restante').textContent = restante;
     }
+    imprimirAlerta(mensaje, tipo) {
+        // Crea el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        // Si es de tipo error agrega una clase
+        if(tipo === 'error') {
+             divMensaje.classList.add('alert-danger');
+        } else {
+             divMensaje.classList.add('alert-success');
+        }
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Insertar en el DOM
+        document.querySelector('.summary').insertBefore(divMensaje, formulario);
+
+        // Quitar el alert despues de 3 segundos
+        setTimeout( () => {
+             document.querySelector('.summary .alert').remove();
+        }, 3000);
+   }
+
 }
 const ui = new UI();
 let presupuesto;
@@ -38,11 +61,53 @@ function preguntarPresupuesto(){
     
     
     if (presupuesto === '' || presupuesto === null || presupuesto <= 0){
-        alert ('Ingrese una cantidad valida');
+        window.location.reload();
     }
 
     presupuesto = new Presupuesto (presupuestoIngresado);
-    console.log(presupuesto);
+    //console.log(presupuesto);
 
     ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+    e.preventDefault();
+
+     // Leer del formulario de Gastos
+     const nombre = document.querySelector('#gasto').value;
+     const cantidad = Number( document.querySelector('#cantidad').value);
+
+     // Comprobar que los campos no esten vacios
+     if(nombre === '' || cantidad === '') {
+          // 2 parametros: mensaje y tipo
+          ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+     } else if(cantidad <= 0 || isNaN(cantidad )) {
+
+          // si hay una cantidad negativa o letras...
+          ui.imprimirAlerta('Cantidad no válida', 'error')
+     } else {
+            const gasto = { nombre, cantidad, id: Date.now() };
+
+            // Añadir nuevo gasto 
+            presupuesto.nuevoGasto(gasto)
+
+            // Insertar en el HTML
+            ui.imprimirAlerta('Correcto', 'correcto');
+
+            // Pasa los gastos para que se impriman...
+            const { gastos} = presupuesto;
+            ui.agregarGastoListado(gastos);
+
+            // Cambiar la clase que nos avisa si se va terminando
+            ui.comprobarPresupuesto(presupuesto);
+
+            // Actualiza el presupuesto restante
+            const { restante } = presupuesto;
+
+            // Actualizar cuanto nos queda
+            ui.actualizarRestante(restante)
+
+            // Reiniciar el form
+            formulario.reset();
+     }
 }
