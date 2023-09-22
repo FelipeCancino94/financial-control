6327519dabae0034fb26439c10cde16e08bac369
lauralop11 <lauralop_11@hotmail.com>
2023-09-21 20:05:28 -0400

    agregar mensaje de error

--- dist/index.js
+++ dist/index.js
@@ -7,11 +7,13 @@ console.log(mensaje);
 const gastoMensual = document.querySelector('#presupuesto');
 const gastoListado = document.querySelector('#nombre-gasto');
 const selectorMes = document.querySelector('#selectMes');
+const formulario = document.getElementById('agregar-gasto');
 
 
 
 function eventListener(){
     selectorMes.addEventListener ('change', preguntarPresupuesto);   
+    formulario.addEventListener('submit', agregarGasto);
 }
 eventListener();
 
@@ -44,11 +46,11 @@ class UI {
         divMensaje.textContent = mensaje;
 
         // Insertar en el DOM
-        document.querySelector('.summary').insertBefore(divMensaje, formulario);
+        document.querySelector('#gastos').insertBefore(divMensaje, formulario);
 
         // Quitar el alert despues de 3 segundos
         setTimeout( () => {
-             document.querySelector('.summary .alert').remove();
+             document.querySelector('.formulario .alert').remove();
         }, 3000);
    }
 
@@ -60,7 +62,7 @@ function preguntarPresupuesto(){
     const presupuestoIngresado = prompt('Cual es el presupuesto?');
     
     
-    if (presupuesto === '' || presupuesto === null || presupuesto <= 0){
+    if (presupuestoIngresado === '' || presupuestoIngresado === null || presupuestoIngresado <= 0){
         window.location.reload();
     }
 
@@ -71,43 +73,26 @@ function preguntarPresupuesto(){
 }
 
 function agregarGasto(e) {
+
     e.preventDefault();
 
      // Leer del formulario de Gastos
-     const nombre = document.querySelector('#gasto').value;
+     const nombre = document.querySelector('#nombre-gasto').value;
      const cantidad = Number( document.querySelector('#cantidad').value);
 
      // Comprobar que los campos no esten vacios
      if(nombre === '' || cantidad === '') {
-          // 2 parametros: mensaje y tipo
           ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
      } else if(cantidad <= 0 || isNaN(cantidad )) {
-
-          // si hay una cantidad negativa o letras...
           ui.imprimirAlerta('Cantidad no válida', 'error')
      } else {
-            const gasto = { nombre, cantidad, id: Date.now() };
-
-            // Añadir nuevo gasto 
+            const gasto = { nombre, cantidad, id: Date.now() }; 
             presupuesto.nuevoGasto(gasto)
 
             // Insertar en el HTML
             ui.imprimirAlerta('Correcto', 'correcto');
 
-            // Pasa los gastos para que se impriman...
-            const { gastos} = presupuesto;
-            ui.agregarGastoListado(gastos);
-
-            // Cambiar la clase que nos avisa si se va terminando
-            ui.comprobarPresupuesto(presupuesto);
-
-            // Actualiza el presupuesto restante
-            const { restante } = presupuesto;
-
-            // Actualizar cuanto nos queda
-            ui.actualizarRestante(restante)
-
             // Reiniciar el form
-            formulario.reset();
+           // formulario.reset();
      }
 }
