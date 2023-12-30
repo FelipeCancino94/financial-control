6f8b7ea5f74f74b5ec3eed6321825b9f3fc8d43c
lauralop11 <lauralop_11@hotmail.com>
2023-12-30 15:59:23 -0500

    color

--- src/components/AddSpent/AddSpent.js
+++ src/components/AddSpent/AddSpent.js
@@ -39,7 +39,7 @@ function AddSpent() {
 
   return (
     <div className="ms-4" >
-      <div className="btn-add-spent z-10 fixed right-2 w-28 flex items-center justify-center text-center text-3xl px-2 py-2 text-white bg-lime-600 rounded-full hover:bg-lime-400 transition ease-in-out mt-3 cursor-pointer" onClick={() => toggleSpentForm()}>
+      <div className="btn-add-spent z-10 fixed right-3 w-28 flex items-center justify-center text-center text-3xl px-2 py-2 text-white bg-lime-400 rounded-full hover:bg-lime-400 transition ease-in-out mt-3 cursor-pointer" onClick={() => toggleSpentForm()}>
         + <span className="text-lg ms-3 hidden">Agregar gasto</span>
       </div>
       <form className="add-spent relative top-16 p-2 mt-3 rounded-lg">
@@ -91,7 +91,7 @@ function AddSpent() {
         </div>
 
         <div className="flex justify-end">
-          <button type="button" className="px-5 py-2 bg-lime-600 text-white rounded-lg hover:bg-lime-400 transition ease-in-out mt-3" onClick={() => addNewSpent()}>
+          <button type="button" className="px-5 py-2 bg-lime-400 text-white rounded-lg hover:bg-lime-600 transition ease-in-out mt-3" onClick={() => addNewSpent()}>
             Agregar
           </button>
         </div>
